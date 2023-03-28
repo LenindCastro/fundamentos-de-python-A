df06a97b7f447c60eefe4a6e1b6c1d959204f521
LenindCastro <lenincastro87@gmail.com>
2023-03-28 08:05:02 -0500

    V1

---
+++ Ejercicio 1 .py
@@ -0,0 +1,10 @@
+"""edad=input('Ingrese su edad: ')
+peso=input('Ingrese su peso: ')
+print('La edad de usted es',edad)
+print('Su peso es',peso,'Kg')"""
+
+#Ejercicio 1
+base=int(input("Ingrese la base del triagulo: "))
+altura=int(input('Ingrese la altura del triagulo: '))
+area=base*altura/2
+print("El area de su triagulo es",area)
